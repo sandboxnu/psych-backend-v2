d230b55b13e527ebb117d17478babdbedd09c76a
Arun Jeevanantham <itsmearun98@gmail.com>
2019-11-13 14:35:04 -0500

    added config and error handling

--- src/app.js
+++ src/app.js
@@ -1,40 +1,61 @@
+const dotenv = require('dotenv');
 const db = require('./queries')
 var express = require('express');
 var app = express();
+dotenv.config();
 app.use(express.json());
 
 app.get('/', function (req, res) {
-    db.getTable().then(data => {
-        res.status(200)
-        res.send("Hello!");
-    })
+    res.status(200);
+    res.send("Hello");
 });
 
 app.get('/config', function (req, res) {
-    res.send('Hello World TEST!');
+    db.getConfig().then(data => {
+        res.status(200);
+        res.send(data)
+    }).catch(e => {
+        res.status(500);
+        res.send("Internal server error. Pleae try again later.")
+    });
 });
 
 app.post('/config', function (req, res) {
-    res.send('Hello World TEST!');
+    if (req.body == null) {
+        res.status(400);
+        res.send('Incorrect request body format');
+    }
+    db.updateConfig(req.body)
+        .then(result => {
+            res.status(200);
+            res.send("Updated successfully");
+        }).catch(e => {
+            res.status(500);
+            res.send("Internal server error. Pleae try again later.")
+        });
 });
 
 app.get('/data', function (req, res) {
-    // TODO: Add error handling
     // TODO: Add function to analyze data -> convert to CSV files per user -> then return zipped package with all files.
     db.getTable().then(data => {
         res.status(200);
         res.send(data);
-    })
+    }).catch(e => {
+        res.status(500);
+        res.send("Internal server error. Pleae try again later.")
+    });
 });
 
 app.get('/data/:userId', function (req, res) {
     var userId = req.params.userId;
-    // TODO: Add error handling
     // TODO: Add function to analyze data and convert to CSV file.
     db.getDataByUser(userId).then(data => {
         res.status(200);
         res.send(data);
-    })
+    }).catch(e => {
+        res.status(500);
+        res.send("Internal server error. Pleae try again later.")
+    });
 });
 
 app.post('/data', function (req, res) {
@@ -59,7 +80,7 @@ app.post('/data', function (req, res) {
             res.send({participantId: req.body.userId});
         }).catch(e => {
             res.status(500);
-            res.send("We dun goofed. Plz try AgAiN ltr.")
+            res.send("Internal server error. Pleae try again later.")
         });
 });
 
--- src/queries.js
+++ src/queries.js
@@ -1,15 +1,64 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const Pool = require('pg').Pool
 const pool = new Pool({
-  user: 'me',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'password',
-  port: 5432,
+  user: process.env.PSYCH_DATABASE_USER,
+  host: process.env.PSYCH_DATABASE_HOST,
+  database: process.env.PSYCH_DATABASE_NAME,
+  password: process.env.PSYCH_DATABASE_PASSWORD,
+  port: process.env.PSYCH_DATABASE_PORT,
 })
+
+async function updateConfig(newConfig) {
+
+  const query = {
+      text: "UPDATE configdata SET configJSON = $1 WHERE configid='default'",
+      values: [JSON.stringify(newConfig)]
+  }
+
+  success = await pool.query(query) 
+    .then(() => {return true})
+    .catch((e) => {
+      console.log(e);
+      return false;
+    });
+  
+  if (!success) {
+    throw new Error("Error updating the db")
+  }
+  return success
+}
+
+async function getConfig() {
+  const result = await pool.query("SELECT * from configdata WHERE configid='default' LIMIT 1")
+    .then((res) => {
+      return res.rows[0];
+    }).catch((e) => {
+      console.log(e);
+      return null;
+    });
+
+    if (result === null) {
+      throw new Error("Error reading from the db")
+    }
+    
+    return result;
+}
 
 async function getTable() {
   const result = await pool.query('SELECT * FROM participantdata ORDER BY id ASC')
-  return result.rows;
+    .then((res) => {
+      return res.rows;
+    }).catch((e) => {
+      console.log(e);
+      return null;
+    });
+
+  if (result === null) {
+    throw new Error("Error reading from the db")
+  }
+  return result;
 }
 
 async function getDataByUser(id) {
@@ -18,25 +67,42 @@ async function getDataByUser(id) {
     values: [id]
   }
   const result = await pool.query(query)
-  return result.rows;
+    .then((res) => {
+      return res.rows;
+    }).catch((e) => {
+      console.log(e);
+      return null;
+    });
+
+  if (result === null) {
+    throw new Error("Error reading from the db")
+  }
+  return result;
 }
 
 async function postData(id, data) {
+  // TODO: Enforce session data is JSON
   const query = {
     text: 'INSERT INTO participantdata(participantid, sessiondata) VALUES($1, $2)',
     values: [id, data]
   }
   success = await pool.query(query) 
     .then(() => {return true})
-    .catch(() => {return false});
+    .catch((e) => {
+      console.log(e);
+      return false;
+    });
   
   if (!success) {
     throw new Error("Error inserting to db")
   }
+  return success;
 }
 
 module.exports = {
     getTable,
     getDataByUser, 
-    postData
+    postData,
+    updateConfig,
+    getConfig,
 }
